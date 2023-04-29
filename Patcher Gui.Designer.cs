99e26e99355a1954570daa5b8be2e72d334183a4
StealthDrifter <53316578+StealthDrifter@users.noreply.github.com>
2023-04-29 01:05:28 -0700

    Add files via upload

---
+++ Patcher Gui.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.Logs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(281, 59);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(146, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Change Cursor Texture";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 63F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(141, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 122);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Patch!";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(281, 84);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(138, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Change Death Sound";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(281, 109);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(85, 19);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Unlock FPS";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Logs
+            // 
+            this.Logs.Location = new System.Drawing.Point(0, 0);
+            this.Logs.Multiline = true;
+            this.Logs.Name = "Logs";
+            this.Logs.ReadOnly = true;
+            this.Logs.Size = new System.Drawing.Size(250, 175);
+            this.Logs.TabIndex = 4;
+            this.Logs.TextChanged += new System.EventHandler(this.Logs_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 319);
+            this.Controls.Add(this.Logs);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Roblox Patcher Made by StealthDrifter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private Button button1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private TextBox Logs;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ namespace WinFormsApp1
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
@@ -31,16 +29,16 @@ namespace WinFormsApp1
                 return;
             }
 
-            if (!Modify(robloxPath, Logs))
+            if (!TryModify(robloxPath, Logs))
             {
                 Logs.Text += "\nSomething went horribly wrong" + Environment.NewLine;
                 string message3 = "Oops! something went wrong! contact developer";
                 string caption3 = "Failure! :(";
                 MessageBoxButtons buttons3 = MessageBoxButtons.OK;
                 _ = MessageBox.Show(message3, caption3, buttons3);
+                return;
             }
             Logs.Text += "\nFinished modifying game files" + Environment.NewLine;
-            Console.WriteLine();
             string message2 = "Roblox has been modified!";
             string caption2 = "Success!";
             MessageBoxButtons buttons2 = MessageBoxButtons.OK;
@@ -49,16 +47,16 @@ namespace WinFormsApp1
 
         private static bool TryFindRoblox(out string robloxPath, TextBox Logs)
         {
-            string adminPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86);
-            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            DirectoryInfo Di = new DirectoryInfo($"{userPath}\\Roblox\\Versions");
+            string adminPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86), @"Roblox\Versions");
+            string userPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Roblox\Versions");
+            DirectoryInfo Di = new DirectoryInfo(userPath);
             try
             {
                 DirectoryInfo[] DiArr = Di.GetDirectories();
             }
             catch
             {
-                Di = new DirectoryInfo($"{adminPath}\\Roblox\\Versions");
+                Di = new DirectoryInfo(adminPath);
                 try
                 {
                     DirectoryInfo[] DiArr = Di.GetDirectories();
@@ -78,7 +76,7 @@ namespace WinFormsApp1
                 Logs.Text += "Checking folders in directory" + Environment.NewLine;
                 foreach (DirectoryInfo dri in diArr)
                 {
-                    if (!File.Exists(dri.FullName + "\\RobloxStudioBeta.exe"))
+                    if (!File.Exists(Path.Combine(dri.FullName, "RobloxStudioBeta.exe")))
                     {
                         DateTime dt = Directory.GetCreationTime(dri.FullName);
                         folderNames[counting] = dri.FullName;
@@ -111,68 +109,43 @@ namespace WinFormsApp1
             return false;
         }
 
-        private static bool Modify(string folderName, TextBox Logs)
+        private static bool TryModify(string folderName, TextBox Logs)
         {
             try
             {
                 if (modifyDeathSound || modifyAll)
                 {
-                    string newSoundFile = ".\\Assets\\ouch.ogg";
-                    string oldSoundFile = folderName + "\\content\\sounds\\ouch.ogg";
-                    string clonedNewSoundFile = ".\\Assets\\ouch2.ogg";
+                    string newSoundFile = @".\Assets\ouch.ogg";
+                    string oldSoundFile = Path.Combine(folderName, @"content\sounds\ouch.ogg");
                     Logs.Text += "Modifying Sound File" + Environment.NewLine;
-                    if (File.Exists(oldSoundFile))
-                    {
-                        File.Delete(oldSoundFile); // deletes old sound file
-                    }
-                    File.Copy(newSoundFile, clonedNewSoundFile); // clones desired sound file
-                    File.Move(newSoundFile, oldSoundFile); // replaces old sound file with desired sound file
-                    File.Move(clonedNewSoundFile, newSoundFile); // Renames cloned desired sound file for later use
+
+                    File.Copy(newSoundFile, oldSoundFile, true); // replaces old sound file with desired sound file
                     Logs.Text += "Finished modifying Death Sound" + Environment.NewLine;
                 }
                 if (modifyCursor || modifyAll)
                 {
-                    string newFarCursor = ".\\Assets\\ArrowFarCursor.png";
-                    string newArrowCursor = ".\\Assets\\ArrowCursor.png";
-                    string clonedNewFarCursor = ".\\Assets\\ArrowFarCursor2.png";
-                    string clonedNewArrowCursor = ".\\Assets\\ArrowCursor2.png";
-                    string oldFarCursor = folderName + "\\content\\textures\\Cursors\\KeyboardMouse\\ArrowFarCursor.png";
-                    string oldArrowCursor = folderName + "\\content\\textures\\Cursors\\KeyboardMouse\\ArrowCursor.png";
+                    string newFarCursor = @".\Assets\ArrowFarCursor.png";
+                    string newArrowCursor = @".\Assets\ArrowCursor.png";
+                    string oldFarCursor = Path.Combine(folderName, @"content\textures\Cursors\KeyboardMouse\ArrowFarCursor.png");
+                    string oldArrowCursor = Path.Combine(folderName, @"content\textures\Cursors\KeyboardMouse\ArrowCursor.png");
                     Logs.Text += "Modifying Cursor Files" + Environment.NewLine;
-                    if (File.Exists(oldFarCursor))
-                    {
-                        File.Delete(oldFarCursor);
-                    }
-                    if (File.Exists(oldArrowCursor))
-                    {
-                        File.Delete(oldArrowCursor);
-                    }
-                    File.Copy(newFarCursor, clonedNewFarCursor);
-                    File.Copy(newArrowCursor, clonedNewArrowCursor);
-                    File.Move(newFarCursor, oldFarCursor);
-                    File.Move(newArrowCursor, oldArrowCursor);
-                    File.Move(clonedNewFarCursor, newFarCursor);
-                    File.Move(clonedNewArrowCursor, newArrowCursor);
+
+                    File.Copy(newFarCursor, oldFarCursor, true);
+                    File.Copy(newArrowCursor, oldArrowCursor, true);
                     Logs.Text += "Finished modifying Cursor" + Environment.NewLine;
                 }
 
                 if (modifyFps || modifyAll)
                 {
-                    string newConfig = ".\\Assets\\ClientAppSettings.json";
-                    string oldConfig = folderName + "\\ClientSettings\\ClientAppSettings.json";
-                    string clonedNewConfig = ".\\Assets\\ClientAppSettings2.json";
+                    string newConfig = @".\Assets\ClientAppSettings.json";
+                    string oldConfig = Path.Combine(folderName, @"ClientSettings\ClientAppSettings.json");
                     Logs.Text += "Modifying config" + Environment.NewLine;
-                    if (!Directory.Exists(folderName + "\\ClientSettings"))
+
+                    if (!Directory.Exists(Path.Combine(folderName, "ClientSettings")))
                     {
-                        Directory.CreateDirectory(folderName + "\\ClientSettings");
+                        Directory.CreateDirectory(Path.Combine(folderName, "ClientSettings"));
                     }
-                    if (File.Exists(oldConfig))
-                    {
-                        File.Delete(oldConfig);
-                    }
-                    File.Copy(newConfig, clonedNewConfig);
-                    File.Move(newConfig, oldConfig);
-                    File.Move(clonedNewConfig, newConfig);
+                    File.Copy(newConfig, oldConfig, true);
                     Logs.Text += "Finished modifying Config" + Environment.NewLine;
                 }
             } catch
